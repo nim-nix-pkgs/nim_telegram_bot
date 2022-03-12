56c31e89912c47635a881715201e29b8ee1c27e9
hugosenari <hugosenari@gmail.com>
2022-03-12 04:24:16 -0300

    chore: re index nim_telegram_bot

---
+++ 0_2_3/flake.nix
@@ -0,0 +1,42 @@
+{
+  description = ''Generic Configurable Telegram Bot for Nim, with builtin basic functionality and Plugins'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nim_telegram_bot-0_2_3.flake = false;
+  inputs.src-nim_telegram_bot-0_2_3.owner = "juancarlospaco";
+  inputs.src-nim_telegram_bot-0_2_3.ref   = "refs/tags/0.2.3";
+  inputs.src-nim_telegram_bot-0_2_3.repo  = "nim-telegram-bot";
+  inputs.src-nim_telegram_bot-0_2_3.type  = "github";
+  
+  inputs."openexchangerates".dir   = "nimpkgs/o/openexchangerates";
+  inputs."openexchangerates".owner = "riinr";
+  inputs."openexchangerates".ref   = "flake-pinning";
+  inputs."openexchangerates".repo  = "flake-nimble";
+  inputs."openexchangerates".type  = "github";
+  inputs."openexchangerates".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."openexchangerates".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."telebot".dir   = "nimpkgs/t/telebot";
+  inputs."telebot".owner = "riinr";
+  inputs."telebot".ref   = "flake-pinning";
+  inputs."telebot".repo  = "flake-nimble";
+  inputs."telebot".type  = "github";
+  inputs."telebot".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."telebot".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nim_telegram_bot-0_2_3"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nim_telegram_bot-0_2_3";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = ''Generic Configurable Telegram Bot for Nim, with builtin basic functionality and Plugins'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."nim_telegram_bot-master".url = "path:./master";
+  inputs."nim_telegram_bot-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nim_telegram_bot-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nim_telegram_bot-0_0_1".url = "path:./0_0_1";
+  inputs."nim_telegram_bot-0_0_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nim_telegram_bot-0_0_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nim_telegram_bot-0_0_2".url = "path:./0_0_2";
+  inputs."nim_telegram_bot-0_0_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nim_telegram_bot-0_0_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nim_telegram_bot-0_2_0".url = "path:./0_2_0";
+  inputs."nim_telegram_bot-0_2_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nim_telegram_bot-0_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nim_telegram_bot-0_2_2".url = "path:./0_2_2";
+  inputs."nim_telegram_bot-0_2_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nim_telegram_bot-0_2_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nim_telegram_bot-0_2_3".url = "path:./0_2_3";
+  inputs."nim_telegram_bot-0_2_3".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nim_telegram_bot-0_2_3".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nim_telegram_bot-0_2_5".url = "path:./0_2_5";
+  inputs."nim_telegram_bot-0_2_5".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nim_telegram_bot-0_2_5".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nim_telegram_bot-0_3_0".url = "path:./0_3_0";
+  inputs."nim_telegram_bot-0_3_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nim_telegram_bot-0_3_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nim_telegram_bot-0_4_0".url = "path:./0_4_0";
+  inputs."nim_telegram_bot-0_4_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nim_telegram_bot-0_4_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."nim_telegram_bot-0_4_5".url = "path:./0_4_5";
+  inputs."nim_telegram_bot-0_4_5".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nim_telegram_bot-0_4_5".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
